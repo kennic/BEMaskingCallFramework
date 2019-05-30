5bf33ec85df20c4213d18a541603a907114e90b0
Nam Kennic <namkennic@me.com>
2019-05-30 15:14:33 +0700

    1.0.0

---
+++ BEMaskingCall.framework/Headers/BEMaskingCall.h
@@ -0,0 +1,30 @@
+//
+//  BEMaskingCall.h
+//  BEMaskingCall
+//
+//  Created by Nam Kennic on 5/21/19.
+//  Copyright © 2019 Be Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BEMaskingCall.
+FOUNDATION_EXPORT double BEMaskingCallVersionNumber;
+
+//! Project version string for BEMaskingCall.
+FOUNDATION_EXPORT const unsigned char BEMaskingCallVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BEMaskingCall/PublicHeader.h>
+
+
+#import <BEMaskingCall/SPManager.h>
+#import <BEMaskingCall/BeCommon.h>
+#import <BEMaskingCall/CallManager.h>
+#import <BEMaskingCall/CallingVCModel.h>
+#import <BEMaskingCall/RideInfoModel.h>
+#import <BEMaskingCall/UserModel.h>
+#import <BEMaskingCall/BeCustomerInfoModel.h>
+#import <BEMaskingCall/CalloutNumberModel.h>
+#import <BEMaskingCall/ConfigMaskingCallModel.h>
+#import <BEMaskingCall/GlobalService.h>
+#import <BEMaskingCall/CallingViewController.h>
---
+++ BEMaskingCall.podspec
@@ -0,0 +1,17 @@
+Pod::Spec.new do |spec|
+
+spec.name         = "BEMaskingCall"
+spec.version      = "1.0.0"
+spec.summary      = "BEMaskingCall for be"
+spec.description  = "This is frameWork allows you to make a voip call"
+spec.homepage     = "https://github.com/kennic/BEMaskingCallFramework.git"
+spec.license      = { :type => "MIT", :text => "The MIT License (MIT) \n Copyright (c) uncleliz <dinhmanhvp@gmail.com> \n Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files" }
+spec.author       = { "uncleliz" => "dinhmanhvp@gmail.com" }
+spec.source       = { :git => "https://github.com/kennic/BEMaskingCallFramework.git", :branch => 'master', :tag => spec.version }
+spec.ios.vendored_frameworks   = 'BEMaskingCall.framework'
+spec.ios.deployment_target = '10.0'
+spec.ios.dependency "AFNetworking"
+spec.ios.dependency "Stringee"
+#spec.static_framework = true
+
+end
