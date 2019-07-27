72b686c72e4997274c5b5085ae538991d1fdff4a
Nam Kennic <namkennic@me.com>
2019-07-27 20:02:16 +0700

    no message

--- BEMaskingCall.framework/Headers/SPManager.h
+++ BEMaskingCall.framework/Headers/SPManager.h
@@ -110,7 +110,7 @@ typedef NS_ENUM(NSInteger, BEMaskingCallFailReason) {
 - (NSArray*) maskingNumber;
 - (NSString*) getNumberMaskWithTrip:(NSString*)tripID withDriverPhoneNumber:(NSString*)driverPhoneNumber;
 - (NSString*) readNumberMasking;
-- (void) fetchMaskingNumberDriverID:(NSString*)driverId engagementID:(NSString*)engagementID needStringeeToken:(BOOL)needStringeeToken completionHandler:(void(^)(id numberPhone, BOOL success))completionHandler;
+- (void) getCallConfigForEngagementID:(NSString*)engagementID driverID:(NSString*)driverId needStringeeToken:(BOOL)needStringeeToken completionHandler:(void(^)(id numberPhone, BOOL success))completionHandler;
 - (void) putRememberCallOption:(NSString*)option completionHandler:(void(^)(BOOL success))completionHandler;
 - (void) updateConfigMaskingCall:(id)responseObject driverID:(NSString*)driverId engagementID:(NSString*)engagementID;
 - (void) connectToStringeeServer;
